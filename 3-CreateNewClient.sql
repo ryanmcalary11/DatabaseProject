3de47388d7efe57575e64ebc425bfa1d90639c3c
Adam Marshall <adam.marshall@kainos>
2023-09-21 15:25:30 +0100

    Commit all files

---
+++ 3-CreateNewClient.sql
@@ -0,0 +1,7 @@
+USE Project_CoryD;
+
+-- Add Client
+INSERT INTO Client (Client_Fname, Client_Lname, Client_Address, Client_Phone)
+                VALUES ('Brain', 'Thomes', '21 Table Road', '07847326384');
+
+SELECT * FROM Client
