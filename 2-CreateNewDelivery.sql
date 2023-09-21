af979ca740e04ba5b106f1049f2cee92cc150842
Adam Marshall <adam.marshall@kainos>
2023-09-21 15:15:55 +0100

    Backlog 2, Add a new delivery employee to the Delivery employee table

---
+++ 2-CreateNewDelivery.sql
@@ -0,0 +1,7 @@
+USE Project_CoryD;
+
+-- Add Delivery Employee
+INSERT INTO Delivery_Employee (Delivery_Fname, Delivery_Lname, Delivery_Salary, Delivery_BankAcc, Delivery_NINum)
+                VALUES ('Charles', 'Gee', '34000', '72881920058', 'BS34732');
+                
+SELECT * FROM Delivery_Employee;
